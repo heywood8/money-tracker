f12ba65b68583330a18902fdeae6cbfc15a4e9cf
heywood8 <lopatinik+github@yandex.ru>
2025-11-28 17:54:36 +0400

    refactor: move out defaults (#58)
    Co-authored-by: Lopatin, Nikita <NIKITA_LOPATIN@homedepot.com>

---
+++ app/defaults/defaultAccounts.js
@@ -0,0 +1,79 @@
+const defaultAccounts = [
+  {
+    name: 'Ameria card',
+    balance: '100000',
+    currency: 'AMD',
+  },
+  {
+    name: 'Ameria account',
+    balance: '5000',
+    currency: 'AMD',
+  },
+  {
+    name: 'Ameria savings',
+    balance: '60000',
+    currency: 'AMD',
+  },
+  {
+    name: 'Cash amd',
+    balance: '2000',
+    currency: 'AMD',
+  },
+  {
+    name: 'Ineco card',
+    balance: '0',
+    currency: 'AMD',
+  },
+  {
+    name: 'T-bank card',
+    balance: '500',
+    currency: 'RUB',
+  },
+  {
+    name: 'T-bank credit card',
+    balance: '0',
+    currency: 'RUB',
+  },
+  {
+    name: 'Card',
+    balance: '0',
+    currency: 'RUB',
+  },
+  {
+    name: 'Cash rub',
+    balance: '0',
+    currency: 'RUB',
+  },
+  {
+    name: 'Card usd',
+    balance: '0',
+    currency: 'USD',
+  },
+  {
+    name: 'Cash usd',
+    balance: '0',
+    currency: 'USD',
+  },
+  {
+    name: 'Card eur',
+    balance: '0',
+    currency: 'EUR',
+  },
+  {
+    name: 'Cash eur',
+    balance: '0',
+    currency: 'EUR',
+  },
+  {
+    name: 'Cash bath',
+    balance: '0',
+    currency: 'THB',
+  },
+  {
+    name: 'Cash liras',
+    balance: '0',
+    currency: 'TRY',
+  },  
+];
+
+export default defaultAccounts;
