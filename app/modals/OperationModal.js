15619f267d6c815d19eb97cdb402d471886ad046
Lopatin, Nikita <NIKITA_LOPATIN@homedepot.com>
2026-01-08 18:43:06 +0400

    fix: calculator color on "Edit operation" modal

--- app/modals/OperationModal.js
+++ app/modals/OperationModal.js
@@ -495,6 +495,7 @@ export default function OperationModal({ visible, onClose, operation, isNew, onD
                       onValueChange={handleAmountChange}
                       colors={colors}
                       placeholder={t('amount')}
+                      containerBackground={colors.card}
                     />
                   </View>
 
