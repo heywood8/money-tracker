7b25af6c18977e74c1838e1e996d68b973d00d01
heywood8 <lopatinik+github@yandex.ru>
2026-02-04 18:28:15 +0400

    chore(main): release penny 0.40.0

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ module.exports = {
   expo: {
     name: APP_NAME,
     slug: 'app',
-    version: '0.39.4', // x-release-please-version
+    version: '0.40.0', // x-release-please-version
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
